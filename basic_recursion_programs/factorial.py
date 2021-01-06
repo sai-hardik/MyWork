2129255ddcb724f1d21d0da34d2d591e8262e059
sai-hardik <hardiktalluru@gmail.com>
2021-01-06 23:17:14 +0530

    Create factorial.py

---
+++ basic_recursion_programs/factorial.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Nov  4 18:59:46 2020
+
+@author: babut
+"""
+def fact(n):
+    return 1 if (n==1 or n==0) else n * fact(n - 1);
+
+n = 0;
+print("factorial of",n,"is",
+fact(n))
